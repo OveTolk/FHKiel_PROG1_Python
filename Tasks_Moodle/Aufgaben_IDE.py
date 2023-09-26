6d7c925fb5fc55bfabbe8bb63c076ce7d27d6044
OveTolk <ost@ingtolk.de>
2023-09-26 10:42:09 +0200

    Added folder and firt task

---
+++ Tasks_Moodle/Aufgaben_IDE.py
@@ -0,0 +1,30 @@
+x = 5
+y = 10
+z = 10
+print(3 + 4)
+# 7
+print("3 + 4")
+# 3+4
+print(9 / 2)
+# 4.5
+print(9 // 2)
+# Integer division wenn man eine ganze Zahl als Ergebnis möchte
+print(9 % 2)
+# Modulo, wenn man den Rest möchte (9)
+print(z)
+# 10, weil z = 10 als Variable
+print(x == 5)
+# Prüfung ob x == 5 also True
+print(x == 6)
+# False weil x ungleich 6
+print(x + y + z)
+print(x == y)
+print(y == z)
+print(z < x)
+print(z > x)
+print(x * y)
+print(x, y, z)
+print(x, y, z, sep=":")
+print(x, y, z, sep="++")
+print(x, y, z, sep="++", end="---")
+print(x, y, z, sep="++", end="---\n")
