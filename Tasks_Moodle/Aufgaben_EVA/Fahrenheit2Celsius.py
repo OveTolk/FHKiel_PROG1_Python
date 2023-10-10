4237311ab0af9568ce99a65b8487cc9dd4d983c0
OveTolk <ost@ingtolk.de>
2023-10-10 10:50:47 +0200

    Added comments

--- Tasks_Moodle/Aufgaben_EVA/Fahrenheit2Celsius.py
+++ Tasks_Moodle/Aufgaben_EVA/Fahrenheit2Celsius.py
@@ -1,7 +1,12 @@
+# Erklärung was das Programm macht
 print("Dieses Programm rechnet Celsius in Fahrenheit und umgekehrt.")
+
+# Eingabe der benötigten Werte
+
 wert = int(input("Geben Sie die Höhe der Temperatur an (ohne Einheit): "))
 einheit = int(input("(1) Fahrenheit / (2) Celsius: "))
 
+# Umrechnung der Werte in die andere Einheit + Ausgabe dieser Werte
 if einheit == 1:
     nebenrechnung = wert - 32
     nebenrechnung2 = 5 / 9 
--- Tasks_Moodle/Aufgaben_EVA/ZinseszinsCalc.py
+++ Tasks_Moodle/Aufgaben_EVA/ZinseszinsCalc.py
@@ -1,11 +1,16 @@
+# Erklärung was das Programm macht
 print("Dieses Programm errechnet den Zinseszins eines Anfangskapitals mit einem konstanten Zins und einer bestimmten Laufzeit.")
+
+# Bestimmung der benötigten Eingaben
 anfangskapital = float(input("Geben Sie das Anfangskapital (€) ein: "))
 zinssatz = float(input("Geben Sie den Zinssatz (%) ein: "))
 laufzeit = int(input("Geben Sie die Laufzeit (in Jahren): "))
 
+# Berechnungen der Werte
 nebenrechnung1 = zinssatz / 100 + 1
 nebenrechnung2 = nebenrechnung1 ** laufzeit
 endkapital = anfangskapital * nebenrechnung2
 zinseszins = endkapital - anfangskapital
 
+# Ausgabe der Werte
 print(f"Mit einem Anfangskapital von {anfangskapital} € , einem Zinssatz von {zinssatz} % und einer Laufzeit von {laufzeit} Jahren ergibt sich ein Zinseszins von {round(zinseszins, 2)} €. Das Endkapital beträgt {round(endkapital, 2)} €.")
