f38d931689c4032f01954608485b310d267e04a1
Ove Tolk <ove.tolk@sus.bbz-rd-eck.de>
2023-10-12 18:22:49 +0200

    Rechnungen vereinfacht

--- Tasks_Moodle/Aufgaben_EVA/Fahrenheit2Celsius.py
+++ Tasks_Moodle/Aufgaben_EVA/Fahrenheit2Celsius.py
@@ -8,12 +8,8 @@ einheit = int(input("(1) Fahrenheit / (2) Celsius: "))
 
 # Umrechnung der Werte in die andere Einheit + Ausgabe dieser Werte
 if einheit == 1:
-    nebenrechnung = wert - 32
-    nebenrechnung2 = 5 / 9 
-    umrechnung =  nebenrechnung2 * nebenrechnung
+    umrechnung = (wert - 32) * (5 / 9)
     print(f"{wert} F ist umgerechnet {round(umrechnung, 2)} °.")
 elif einheit == 2:
-    nebenrechnung  = 9 / 5 
-    nebenrechnung2 = wert * nebenrechnung
-    umrechnung = nebenrechnung2 + 32
+    umrechnung = (wert * (9 / 5)) + 32
     print(f"{wert} ° ist umgerechnet {round(umrechnung, 2)} F")
