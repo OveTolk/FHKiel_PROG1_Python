7a1b25904e199d984798bf6aace8b852fcdf9089
OveTolk <ost@ingtolk.de>
2023-10-14 23:30:27 +0200

    Deutsche Variablen

--- code_examples/NAHSH_Verkehr/einsteiger_aussteiger.py
+++ code_examples/NAHSH_Verkehr/einsteiger_aussteiger.py
@@ -7,34 +7,34 @@ import pandas as pd
 data = pd.read_csv("code_examples\\NAHSH_Verkehr\\ein_aussteiger.csv", delimiter=";")
 
 # Suchen und Anzeigen der Haltestelle
-def search_haltestelle():
-    search_text = search_entry.get()
-    result = data[data['haltestelle'].str.contains(search_text, case=False)]
-    haltestellen = result['haltestelle'].unique()
+def suche_haltestelle():
+    suchtext = eingabe_haltestelle.get()
+    ergebnis = data[data['haltestelle'].str.contains(suchtext, case=False)]
+    haltestellen = ergebnis['haltestelle'].unique()
 
-    haltestelle_menu['values'] = tuple(haltestellen)
-    haltestelle_menu.set(haltestellen[0])
+    auswahl_haltestelle['values'] = tuple(haltestellen)
+    auswahl_haltestelle.set(haltestellen[0])
 
-    result_text.config(state="normal")
-    result_text.delete(1.0, tk.END)
-    result_text.insert(tk.END, result.to_string(index=False))
-    result_text.config(state="disabled")
+    ausgabe_text.config(state="normal")
+    ausgabe_text.delete(1.0, tk.END)
+    ausgabe_text.insert(tk.END, ergebnis.to_string(index=False))
+    ausgabe_text.config(state="disabled")
 
 # Erstellen des Graphen
-def create_graph():
-    haltestelle = selected_haltestelle.get()
-    selected_day = selected_day_var.get()
-    haltestelle_data = data[(data['haltestelle'] == haltestelle) & (data['tage'] == selected_day)]
-    years = haltestelle_data['jahr']
+def erstelle_graph():
+    haltestelle = auswahl_haltestelle.get()
+    ausgewaehlter_tag = ausgewaehlter_tag_var.get()
+    haltestelle_data = data[(data['haltestelle'] == haltestelle) & (data['tage'] == ausgewaehlter_tag)]
+    jahre = haltestelle_data['jahr']
     einsteiger = haltestelle_data['durchschnitt_einsteiger_tag'].str.replace('.', '').str.replace(',', '.').astype(float)
     aussteiger = haltestelle_data['durchschnitt_aussteiger_tag'].str.replace('.', '').str.replace(',', '.').astype(float)
 
     plt.figure(figsize=(10, 6))
-    plt.plot(years, einsteiger, label='Durchschnitt Einsteiger')
-    plt.plot(years, aussteiger, label='Durchschnitt Aussteiger')
+    plt.plot(jahre, einsteiger, label='Durchschnitt Einsteiger')
+    plt.plot(jahre, aussteiger, label='Durchschnitt Aussteiger')
     plt.xlabel('Jahr')
     plt.ylabel('Durchschnittliche Ein- und Aussteiger')
-    plt.title(f'Durchschnittliche Ein- und Aussteiger für {haltestelle} ({selected_day})')
+    plt.title(f'Durchschnittliche Ein- und Aussteiger für {haltestelle} ({ausgewaehlter_tag})')
     plt.legend()
     plt.grid(True)
     plt.show()
@@ -43,38 +43,32 @@ def create_graph():
 app = tk.Tk()
 app.title("Haltestellensuche und Graph")
 
-search_label = ttk.Label(app, text="Suche nach Haltestelle:")
-search_label.pack(padx=10, pady=5)
+such_label = ttk.Label(app, text="Suche nach Haltestelle:")
+such_label.pack(padx=10, pady=5)
 
-search_entry = ttk.Entry(app)
-search_entry.pack(padx=10, pady=5)
+eingabe_haltestelle = ttk.Entry(app)
+eingabe_haltestelle.pack(padx=10, pady=5)
 
-search_button = ttk.Button(app, text="Suchen", command=search_haltestelle)
-search_button.pack(padx=10, pady=5)
+suchen_button = ttk.Button(app, text="Suchen", command=suche_haltestelle)
+suchen_button.pack(padx=10, pady=5)
 
-result_text = tk.Text(app, height=10, width=40)
-result_text.config(state="disabled")
-result_text.pack(padx=10, pady=5)
+ausgabe_text = tk.Text(app, height=10, width=40)
+ausgabe_text.config(state="disabled")
+ausgabe_text.pack(padx=10, pady=5)
 
-selected_haltestelle = tk.StringVar()
-selected_haltestelle.set(data['haltestelle'].values[0])
+auswahl_haltestelle = ttk.Combobox(app, textvariable=selected_haltestelle, values=data['haltestelle'].unique())
+auswahl_haltestelle.pack(padx=10, pady=5)
 
-haltestelle_label = ttk.Label(app, text="Wähle eine Haltestelle:")
-haltestelle_label.pack(padx=10, pady=5)
+ausgewaehlter_tag_var = tk.StringVar()
+ausgewaehlter_tag_var.set("Mo-Fr")  # Standardauswahl
 
-haltestelle_menu = ttk.Combobox(app, textvariable=selected_haltestelle, values=data['haltestelle'].unique())
-haltestelle_menu.pack(padx=10, pady=5)
+tag_label = ttk.Label(app, text="Wähle einen Tag:")
+tag_label.pack(padx=10, pady=5)
 
-selected_day_var = tk.StringVar()
-selected_day_var.set("Mo-Fr")  # Default selection
+tag_auswahl = ttk.Combobox(app, textvariable=ausgewaehlter_tag_var, values=["Mo-Fr", "Mo-So", "Sa", "So"])
+tag_auswahl.pack(padx=10, pady=5)
 
-day_label = ttk.Label(app, text="Wähle einen Tag:")
-day_label.pack(padx=10, pady=5)
-
-day_menu = ttk.Combobox(app, textvariable=selected_day_var, values=["Mo-Fr", "Mo-So", "Sa", "So"])
-day_menu.pack(padx=10, pady=5)
-
-create_graph_button = ttk.Button(app, text="Graph erstellen", command=create_graph)
-create_graph_button.pack(padx=10, pady=5)
+graph_erstellen_button = ttk.Button(app, text="Graph erstellen", command=erstelle_graph)
+graph_erstellen_button.pack(padx=10, pady=5)
 
 app.mainloop()
