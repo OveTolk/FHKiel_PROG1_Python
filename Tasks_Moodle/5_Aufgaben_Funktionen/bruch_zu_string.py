73e9c0a299e88f96f5e1641565e04c8b32cf3579
OveTolk <ost@ingtolk.de>
2023-10-25 12:47:45 +0200

    Kommentar & Ü2

--- Tasks_Moodle/5_Aufgaben_Funktionen/bruch_zu_string.py
+++ Tasks_Moodle/5_Aufgaben_Funktionen/bruch_zu_string.py
@@ -1,8 +1,17 @@
 # Funktion erstellen, die einen Zähler und Nenner darstellt
+def bruch_zu_string(a):
+        text1, text2 = a
+        print(f"{text1}" + "/" + f"{text2}")
 
-def bruch_zu_string(a, b):
-        print(f"{a}" + "/" + f"{b}")
+# Eingabe des Benutzers
+def inputUser():
+    a = input("Geben Sie den Zähler ein: ")
+    b = input("Geben Sie den Nenner ein: ")
+    return(a,b)
 
-a = input("Geben Sie den Zähler ein: ")
-b = input("Geben Sie den Nenner ein: ")
-bruch_zu_string(a, b)
+# Haupt Funktion
+def main():
+    vars = inputUser()
+    bruch_zu_string(vars)
+
+main()
